3663b1625421317b452cf75e3cac853bf5d6cbe8
J.D. Zamfirescu <zamfi@alum.mit.edu>
2019-03-22 10:04:43 -0700

    making full example for arduino

--- arduino-example/arduino-example.ino
+++ arduino-example/arduino-example.ino
@@ -1,10 +1,17 @@
-// in setup: Serial.begin(115200);
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  // nothing to do here yet
+}
 
 void serialEvent() {
   String input = Serial.readStringUntil('\n');
   input.trim();
 
   if (input == "forward") {
+    Serial.println("FORWARD!");
     // go forward
   } else if (input == "left") {
     // go left
